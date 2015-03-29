4188ccb5110bfd6dcc1d8b072eeb336b6b2ed8b3
Sankar <sankarnarayandatta@gmail.com>
2015-03-29 01:00:28 +0100

    login page added

--- mainServer.py
+++ mainServer.py
@@ -1,6 +1,14 @@
-import Settings
 import tornado.web
 import tornado.httpserver
+import Settings
+
+class loginHandler(tornado.web.RequestHandler):
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        print username, password
+        self.render("index.html")
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -9,7 +17,8 @@ class MainHandler(tornado.web.RequestHandler):
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/", MainHandler)
+            (r"/login/?", MainHandler),
+            (r"/signin/?", loginHandler)
         ]
         settings = {
             "template_path": Settings.TEMPLATE_PATH,
