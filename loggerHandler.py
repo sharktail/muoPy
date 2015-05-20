479e955478523f12cc28f67b17a92a25a159cf6e
Sankar <sankarnarayandatta@gmail.com>
2015-05-20 22:47:55 +0200

    Modified and and logger.conf

--- loggerHandler.py
+++ loggerHandler.py
@@ -1,10 +1,24 @@
 import logging
 import Settings
+import logging.config
 
 class logger(object):
     def __init__(self, name = Settings.mainLogger):
-        logging.basicConfig(filename = Settings.loggerPath + name, level=logging.DEBUG)
+        logging.basicConfig(filename = Settings.loggerPath + name, level=logging.DEBUG, format='%(asctime)s :: %(message)s')
     
-    def info(self):
+    def writeInfo(self, msg):
+        logging.info(msg)
         
+    def writeDebug(self, msg):
+        logging.debug(msg)
+    
+    def writeWarning(self, msg):
+        logging.warning(msg)
+
+class logConfig(object):
+    def __init__(self):
         
+        logging.config.fileConfig(Settings.loggerPath + 'logging.conf')
+
+        # create logger
+        logger = logging.getLogger('simpleExample')
