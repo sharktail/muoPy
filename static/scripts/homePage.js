a464d021c020a976752845c4898106f0cd4f19e2
Sankar <sankarnarayandatta@gmail.com>
2015-08-16 19:13:56 +0200

    Homepage css modified

--- static/scripts/homePage.js
+++ static/scripts/homePage.js
@@ -1,3 +1,12 @@
+function toggleClass(idOne, idTwo)
+{
+	var idOne = document.getElementById(idOne);
+	var idTwo = document.getElementById(idTwo);
+	var cl = idOne.className;
+	idOne.className = idTwo.className;
+	idTwo.className = cl;
+}
+
 
 function toggle_visibility(showId, hideId) 
 {
@@ -5,4 +14,6 @@ function toggle_visibility(showId, hideId)
 	var hide = document.getElementById(hideId);
 	show.style.display = 'block';
 	hide.style.display = 'none';
+	
+	toggleClass("signInBtnId", "signUpBtnId");
 }
