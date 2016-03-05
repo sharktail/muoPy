4ebb04b1eb135d7e1b2848cf8771fb26047f38ba
Sankar <sankarnarayandatta@gmail.com>
2016-03-05 15:57:28 +0100

    settings change for _bcg to _mpc

---
+++ Settings.py
@@ -0,0 +1,24 @@
+import os
+import uuid
+dirname = os.path.dirname(__file__)
+
+STATIC_PATH = os.path.join(dirname, 'static')
+TEMPLATE_PATH = os.path.join(dirname, 'templates')
+COOKIE_SECRET = str(uuid.uuid4())
+UPLOAD_LOCATION = "uploads/"
+DAT_FILE_LOCATION = "datFiles/"
+PRB_FILE_LOCATION = "prbFiles/"
+DOWNLOAD_LOCATION = "/static/downloads/"
+host = "localhost"
+user = "root"
+passwd = "narayan"
+db = "muoPy"#"muoPy"
+
+Prefix = "mpc"
+muoPrefix = "_" + Prefix
+
+#Logger info
+loggerPath = "loggers/"
+mainLogger = "mainLogger.log"
+
+portNumber = 8888
